d440841eb03df4d478d6997a6da037e0c0d7eb49
Jose Ramirez <josemramirez81@outlook.com>
2020-11-05 00:45:53 -0500

    Added admin flag to User model, and seeded the first admin user

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "test#{n}@example"}
     password { "password" }
+
+    trait :admin do
+      admin { true }
+    end
   end
 end
