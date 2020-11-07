82b36b65d71b685ff865b116c1b2a4f5013451e8
Jose Ramirez <josemramirez81@outlook.com>
2020-11-07 10:54:07 -0500

    Only show 'Edit Project' if the user can update the project

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -27,6 +27,12 @@ RSpec.feature "Users can only see the appropriate links" do
       visit project_path(project)
       expect(page).not_to have_link "Delete Project"
     end
+
+    scenario "cannot see the Edit Project link" do
+      visit project_path(project)
+      expect(page).not_to have_link "Edit Project"
+    end
+
   end
 
   context "admin users" do
@@ -36,9 +42,14 @@ RSpec.feature "Users can only see the appropriate links" do
       expect(page).to have_link "New Project"
     end
 
-    scenario "cannot see the Delete Project link" do
+    scenario "can see the Delete Project link" do
       visit project_path(project)
       expect(page).to have_link "Delete Project"
+    end
+
+    scenario "can see the Edit Project link" do
+      visit project_path(project)
+      expect(page).to have_link "Edit Project"
     end
   end
 
